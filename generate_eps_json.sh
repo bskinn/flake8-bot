3463066e84300fed06a930f622596d27be01e6c0
Brian Skinn <brian.skinn@gmail.com>
2023-01-23 23:59:40 -0500

    Add 'set -o' to .sh's and workflow trace option

--- generate_eps_json.sh
+++ generate_eps_json.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
-# TODO: Add bash best practices
+
+#####################################################################
+# Per https://sharats.me/posts/shell-script-best-practices/ (partial)
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [[ "${TRACE-0}" == "1" ]]; then
+    set -o xtrace
+fi
+#####################################################################
+
+
 DATA_FILE="data/f8_active.list"
 LOG_FILE="data/gen.log"
 
