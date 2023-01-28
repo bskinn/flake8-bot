8da6d78920af27f27c98dfc05d20e246ba8bae99
Brian Skinn <brian.skinn@gmail.com>
2023-01-28 10:26:29 -0500

    Start refactor of pkg-id/tweet/RSS code
    This should *really* have been implemented separately from the start,
    though it *was* simpler at the time to just shoehorn everything
    together.

--- create_tweets.py
+++ create_tweets.py
@@ -14,9 +14,9 @@ from packaging.version import Version
 
 osenv = os.environ.get
 
-EpsPair = namedtuple("EpsPair", ["new", "old"])
+# EpsPair = namedtuple("EpsPair", ["new", "old"])
 
-TIMESTAMP = arrow.utcnow().timestamp()
+# TIMESTAMP = arrow.utcnow().timestamp()
 
 NEW_PKG_MSG = dedent(
     """\
@@ -77,13 +77,13 @@ def get_params():
     return vars(ns)
 
 
-def get_eps():
-    eps_rep = json.loads(Path("data", "eps_rep.json").read_text())
-    eps_rep_old = json.loads(Path("data", "eps_rep.json.old").read_text())
-    eps_ext = json.loads(Path("data", "eps_ext.json").read_text())
-    eps_ext_old = json.loads(Path("data", "eps_ext.json.old").read_text())
+# def get_eps():
+#     eps_rep = json.loads(Path("data", "eps_rep.json").read_text())
+#     eps_rep_old = json.loads(Path("data", "eps_rep.json.old").read_text())
+#     eps_ext = json.loads(Path("data", "eps_ext.json").read_text())
+#     eps_ext_old = json.loads(Path("data", "eps_ext.json.old").read_text())
 
-    return EpsPair(eps_rep, eps_rep_old), EpsPair(eps_ext, eps_ext_old)
+#     return EpsPair(eps_rep, eps_rep_old), EpsPair(eps_ext, eps_ext_old)
 
 
 def get_rss_json():
@@ -102,8 +102,8 @@ def get_rss_json():
     return rss_json
 
 
-def write_rss_json(rss_json):
-    Path("data", "rss.json").write_text(json.dumps(rss_json))
+# def write_rss_json(rss_json):
+#     Path("data", "rss.json").write_text(json.dumps(rss_json))
 
 
 def get_api():
@@ -121,20 +121,20 @@ def tweet_upd_package(api, *, pkg, version, summary):
     api.update_status(UPD_PKG_MSG.format(pkg=pkg, version=version, summary=summary))
 
 
-def set_new_packages(eps_pair):
-    return {pkg for pkg in eps_pair.new if pkg not in eps_pair.old}
+# def set_new_packages(eps_pair):
+#     return {pkg for pkg in eps_pair.new if pkg not in eps_pair.old}
 
 
-def set_upd_packages(eps_pair):
-    return {
-        pkg
-        for pkg in eps_pair.old
-        if pkg in eps_pair.new
-        and (
-            Version(eps_pair.new[pkg]["version"])
-            > Version(eps_pair.old[pkg]["version"])
-        )
-    }
+# def set_upd_packages(eps_pair):
+#     return {
+#         pkg
+#         for pkg in eps_pair.old
+#         if pkg in eps_pair.new
+#         and (
+#             Version(eps_pair.new[pkg]["version"])
+#             > Version(eps_pair.old[pkg]["version"])
+#         )
+#     }
 
 
 def get_do_post(params):
@@ -148,19 +148,19 @@ def main():
 
     api = get_api() if do_post else None
 
-    eps_pair_rep, eps_pair_ext = get_eps()
+    # eps_pair_rep, eps_pair_ext = get_eps()
 
     rss_json = get_rss_json()
 
-    new_pkgs = set.union(*map(set_new_packages, (eps_pair_rep, eps_pair_ext)))
-    upd_pkgs = set.union(*map(set_upd_packages, (eps_pair_rep, eps_pair_ext)))
+    # new_pkgs = set.union(*map(set_new_packages, (eps_pair_rep, eps_pair_ext)))
+    # upd_pkgs = set.union(*map(set_upd_packages, (eps_pair_rep, eps_pair_ext)))
 
     if len(new_pkgs | upd_pkgs) < 1:
         print("No packages to tweet about.")
 
     for pkg in new_pkgs:
         print(f"**** Tweeting new package: {pkg} ****")
-        pkg_data = eps_pair_rep.new.get(pkg, eps_pair_ext.new[pkg])
+        # pkg_data = eps_pair_rep.new.get(pkg, eps_pair_ext.new[pkg])
 
         if do_post:
             tweet_new_package(
@@ -170,19 +170,19 @@ def main():
         else:
             print(f"Would tweet {pkg} v{pkg_data['version']}")
 
-        rss_json.append(
-            {
-                "timestamp": TIMESTAMP,
-                "pkg": pkg,
-                "version": pkg_data["version"],
-                "summary": pkg_data["summary"],
-                "status": "new",
-            }
-        )
+        # rss_json.append(
+        #     {
+        #         "timestamp": TIMESTAMP,
+        #         "pkg": pkg,
+        #         "version": pkg_data["version"],
+        #         "summary": pkg_data["summary"],
+        #         "status": "new",
+        #     }
+        # )
 
     for pkg in upd_pkgs:
         print(f"**** Tweeting updated package: {pkg} ****")
-        pkg_data = eps_pair_rep.new.get(pkg, eps_pair_ext.new[pkg])
+        # pkg_data = eps_pair_rep.new.get(pkg, eps_pair_ext.new[pkg])
 
         if do_post:
             tweet_upd_package(
@@ -192,17 +192,17 @@ def main():
         else:
             print(f"Would tweet {pkg} v{pkg_data['version']}")
 
-        rss_json.append(
-            {
-                "timestamp": TIMESTAMP,
-                "pkg": pkg,
-                "version": pkg_data["version"],
-                "summary": pkg_data["summary"],
-                "status": "updated",
-            }
-        )
+        # rss_json.append(
+        #     {
+        #         "timestamp": TIMESTAMP,
+        #         "pkg": pkg,
+        #         "version": pkg_data["version"],
+        #         "summary": pkg_data["summary"],
+        #         "status": "updated",
+        #     }
+        # )
 
-    write_rss_json(rss_json)
+    # write_rss_json(rss_json)
 
 
 if __name__ == "__main__":
---
+++ extract_new_upd_pkgs.py
@@ -0,0 +1,80 @@
+import json
+from collections import namedtuple
+from pathlib import Path
+
+import arrow
+from packaging.version import Version
+
+
+EpsPair = namedtuple("EpsPair", ["new", "old"])
+
+TIMESTAMP = arrow.utcnow().timestamp()
+
+def get_eps():
+    eps_rep = json.loads(Path("data", "eps_rep.json").read_text())
+    eps_rep_old = json.loads(Path("data", "eps_rep.json.old").read_text())
+    eps_ext = json.loads(Path("data", "eps_ext.json").read_text())
+    eps_ext_old = json.loads(Path("data", "eps_ext.json.old").read_text())
+
+    return EpsPair(eps_rep, eps_rep_old), EpsPair(eps_ext, eps_ext_old)
+
+
+def set_new_packages(eps_pair):
+    return {pkg for pkg in eps_pair.new if pkg not in eps_pair.old}
+
+
+def set_upd_packages(eps_pair):
+    return {
+        pkg
+        for pkg in eps_pair.old
+        if pkg in eps_pair.new
+        and (
+            Version(eps_pair.new[pkg]["version"])
+            > Version(eps_pair.old[pkg]["version"])
+        )
+    }
+
+def write_new_upd_json(pkg_json):
+    Path("data", "new_upd_pkgs.json").write_text(json.dumps(pkg_json))
+    
+    
+def main():
+    
+    new_upd_pkg_json = []
+    
+    eps_pair_rep, eps_pair_ext = get_eps()
+    
+    new_pkgs = set.union(*map(set_new_packages, (eps_pair_rep, eps_pair_ext)))
+    upd_pkgs = set.union(*map(set_upd_packages, (eps_pair_rep, eps_pair_ext)))
+    
+    for pkg in new_pkgs:
+        pkg_data = eps_pair_rep.new.get(pkg, eps_pair_ext.new[pkg])
+        
+        new_upd_pkg_json.append(
+            {
+                "timestamp": TIMESTAMP,
+                "pkg": pkg,
+                "version": pkg_data["version"],
+                "summary": pkg_data["summary"],
+                "status": "new",
+            }
+        )
+    
+    for pkg in upd_pkgs:
+        pkg_data = eps_pair_rep.new.get(pkg, eps_pair_ext.new[pkg])
+        
+        new_upd_pkg_json.append(
+            {
+                "timestamp": TIMESTAMP,
+                "pkg": pkg,
+                "version": pkg_data["version"],
+                "summary": pkg_data["summary"],
+                "status": "updated",
+            }
+        )
+    
+    write_new_upd_json(new_upd_pkg_json)
+
+if __name__ == "__main__":
+    main()
+    
