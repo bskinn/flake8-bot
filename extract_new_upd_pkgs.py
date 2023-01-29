a7a701e3ed1ceac5b93a30b88eedf01f929cc077
Brian Skinn <brian.skinn@gmail.com>
2023-01-29 10:52:20 -0500

    Apply pre-commit cleanup

--- extract_new_upd_pkgs.py
+++ extract_new_upd_pkgs.py
@@ -10,6 +10,7 @@ EpsPair = namedtuple("EpsPair", ["new", "old"])
 
 TIMESTAMP = arrow.utcnow().timestamp()
 
+
 def get_eps():
     eps_rep = json.loads(Path("data", "eps_rep.json").read_text())
     eps_rep_old = json.loads(Path("data", "eps_rep.json.old").read_text())
@@ -34,22 +35,23 @@ def set_upd_packages(eps_pair):
         )
     }
 
+
 def write_new_upd_json(pkg_json):
     Path("data", "new_upd_pkgs.json").write_text(json.dumps(pkg_json))
-    
-    
+
+
 def main():
-    
+
     new_upd_pkg_json = []
-    
+
     eps_pair_rep, eps_pair_ext = get_eps()
-    
+
     new_pkgs = set.union(*map(set_new_packages, (eps_pair_rep, eps_pair_ext)))
     upd_pkgs = set.union(*map(set_upd_packages, (eps_pair_rep, eps_pair_ext)))
-    
+
     for pkg in new_pkgs:
         pkg_data = eps_pair_rep.new.get(pkg, eps_pair_ext.new[pkg])
-        
+
         new_upd_pkg_json.append(
             {
                 "timestamp": TIMESTAMP,
@@ -59,10 +61,10 @@ def main():
                 "status": "new",
             }
         )
-    
+
     for pkg in upd_pkgs:
         pkg_data = eps_pair_rep.new.get(pkg, eps_pair_ext.new[pkg])
-        
+
         new_upd_pkg_json.append(
             {
                 "timestamp": TIMESTAMP,
@@ -72,9 +74,9 @@ def main():
                 "status": "updated",
             }
         )
-    
+
     write_new_upd_json(new_upd_pkg_json)
+
 
 if __name__ == "__main__":
     main()
-    
