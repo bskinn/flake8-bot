cc8fac9c2cdeaef04b79a941a8806b84cbefceaa
Brian Skinn <brian.skinn@gmail.com>
2023-02-22 23:25:25 -0500

    Add PEP503 norm to new/upd package checking
    If a package gets renamed to within identical PEP503 normalization,
    the version tracking should be able to follow this rename.
    
    This rev implements this, as confirmed by flufl.flake8 being correctly
    treated as an update to flufl-flake8.
    
    Curiously, both flufl.flake8 and flufl-flake8 are appearing in
    the rendered Markdown tables. Seems like flufl-flake8 should now
    not be showing up there...

--- extract_new_upd_pkgs.py
+++ extract_new_upd_pkgs.py
@@ -1,4 +1,5 @@
 import json
+import re
 from collections import namedtuple
 from pathlib import Path
 
@@ -9,6 +10,17 @@ from packaging.version import Version
 EpsPair = namedtuple("EpsPair", ["new", "old"])
 
 TIMESTAMP = arrow.utcnow().timestamp()
+
+
+def pep503_norm(pkg_name):
+    """Apply PEP503 normalization to the given package name.
+
+    Recipe per PEP 503:
+
+    https://peps.python.org/pep-0503/#normalized-names
+
+    """
+    return re.sub("[-_.]+", "-", pkg_name).lower()
 
 
 def get_eps():
@@ -23,18 +35,30 @@ def get_eps():
 # For these two functions, we assert that if a maintainer *deletes*
 # a package from PyPI, they are communicating an intent to abandon
 # any flake8 error codes they project had once claimed.
-def set_new_packages(eps_pair):
-    return {pkg for pkg in eps_pair.new if pkg not in eps_pair.old}
-
-
-def set_upd_packages(eps_pair):
+def make_set_new_packages(eps_pair):
     return {
         pkg
         for pkg in eps_pair.new
-        if pkg in eps_pair.old
+        if pep503_norm(pkg) not in {pep503_norm(old_pkg) for old_pkg in eps_pair.old}
+    }
+
+
+def make_set_upd_packages(eps_pair):
+    eps_pair_old_reverse_pep503_map = {
+        pep503_norm(old_pkg): old_pkg for old_pkg in eps_pair.old
+    }
+
+    return {
+        new_pkg
+        for new_pkg in eps_pair.new
+        if pep503_norm(new_pkg) in {pep503_norm(old_pkg) for old_pkg in eps_pair.old}
         and (
-            Version(eps_pair.new[pkg]["version"])
-            > Version(eps_pair.old[pkg]["version"])
+            Version(eps_pair.new[new_pkg]["version"])
+            > Version(
+                eps_pair.old[eps_pair_old_reverse_pep503_map[pep503_norm(new_pkg)]][
+                    "version"
+                ]
+            )
         )
     }
 
@@ -49,8 +73,8 @@ def main():
 
     eps_pair_rep, eps_pair_ext = get_eps()
 
-    new_pkgs = set.union(*map(set_new_packages, (eps_pair_rep, eps_pair_ext)))
-    upd_pkgs = set.union(*map(set_upd_packages, (eps_pair_rep, eps_pair_ext)))
+    new_pkgs = set.union(*map(make_set_new_packages, (eps_pair_rep, eps_pair_ext)))
+    upd_pkgs = set.union(*map(make_set_upd_packages, (eps_pair_rep, eps_pair_ext)))
 
     for pkg in new_pkgs:
         pkg_data = eps_pair_rep.new.get(pkg, eps_pair_ext.new[pkg])
